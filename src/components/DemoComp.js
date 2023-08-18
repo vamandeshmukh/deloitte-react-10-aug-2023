0ef05bb883b7ad1c6c3dcee33baee96fcc61f748
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-18 15:08:41 +0530

    react

--- src/components/DemoComp.js
+++ src/components/DemoComp.js
@@ -1,16 +1,30 @@
+class DemoComp {
 
-const DemoComp = () => {
-
-    return (
-        <div>
-            <h1>Demo Component</h1>
-            <p>This is demo component.</p>
-        </div>
-    );
-
+    render() {
+        return (
+            <div>
+                <h1>Demo Component</h1>
+                <p>This is demo component.</p>
+            </div>
+        );
+    }
 };
 
 export default DemoComp;
+
+
+// const DemoComp = () => {
+
+//     return (
+//         <div>
+//             <h1>Demo Component</h1>
+//             <p>This is demo component.</p>
+//         </div>
+//     );
+
+// };
+
+// export default DemoComp;
 
 // function DemoComp() {
 
