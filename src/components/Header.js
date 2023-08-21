913862a758bc2ce93ee8465ec87f91385dc6b8a2
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-21 17:08:08 +0530

    react

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 
 const Header = () => {
+
+    // use this navbar for proper styling - 
+    // https://getbootstrap.com/docs/5.3/components/navbar/
+
     return (
         <div>
-            <Link to='/home'>Home</Link>
+            <Link to='/home'><img height={'30px'} src="https://logodix.com/logo/64729.png" /> </Link>
             <Link to='/demo'>Demo</Link>
             <Link to='/parent'>Parent</Link>
 
