be02dc9673d8fc257ece354edd7434dd1651b6bf
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-21 18:30:39 +0530

    react

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,27 +17,27 @@ const Header = () => {
 
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/"><img height={'30px'} src="https://logodix.com/logo/64729.png" /></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/"><img height={'30px'} src="https://logodix.com/logo/64729.png" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="/demo">Demo</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/demo">Demo</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="/parent">Parent</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/parent">Parent</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">CompN</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">CompN</a>
                             </li>
                         </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button class="btn btn-outline-primary" type="submit">Search</button>
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" placeholder="Search" />
+                            <button className="btn btn-outline-primary" type="submit">Search</button>
                         </form>
                     </div>
                 </div>
