345815627a29c23b1b60c764eb1834632c9ec513
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-21 11:54:31 +0530

    react

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 
 const Child = (props) => {
+
+    const [childData, setChildData] = useState('Monu');
+
+    const sendDataToParent = () => {
+        props.childDataToParent(childData);
+    };
+
     return (
         <div>
             <h1>Child Component</h1>
-            <p>Parent data in child component: {props.parentDataToChild} </p>
+            <p>Parent data in child component: {props.parentDataToChild}</p>
+            <p>Child data in child component: {childData}</p>
+            <button onClick={sendDataToParent}>Send Data To Parent</button>
         </div>
     );
 };
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -4,12 +4,18 @@ import Child from "./Child";
 const Parent = () => {
 
     const [parentData, setParentData] = useState('Sonu');
+    const [childDataInParent, setChildDataInParent] = useState('');
+
+    const getChildData = (arg) => {
+        setChildDataInParent(arg);
+    };
 
     return (
         <div>
             <h1>Parent Component</h1>
             <p>Parent data in parent component: {parentData} </p>
-            <Child parentDataToChild={parentData} />
+            <p>Child data in parent component: {childDataInParent} </p>
+            <Child parentDataToChild={parentData} childDataToParent={getChildData} />
         </div>
     );
 };
