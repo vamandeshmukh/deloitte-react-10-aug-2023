8b8cc4619ebb9979a32deceb2fd641a13f64af57
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-23 11:14:49 +0530

    react

--- src/components/Child2.js
+++ src/components/Child2.js
@@ -1,37 +1,37 @@
-// import { useEffect, useState } from "react";
-
-// const Child2 = (props) => {
-
-//     const [child2Data, setChild2Data] = useState();
-
-//     useEffect(() => {
-//         console.log(props);
-//         setChild2Data(props.ch1ToCh2);
-//     },
-//         [props]);
-
-//     return (
-//         <div>
-//             <h1>Child2 Component</h1>
-//             <p>Child data in child2 component: {child2Data}</p>
-//         </div>
-//     );
-// };
-
-// export default Child2;
-
+import { useEffect, useState } from "react";
 
 const Child2 = (props) => {
+
+    const [child2Data, setChild2Data] = useState('');
+
+    useEffect(() => {
+        console.log(props);
+        setChild2Data(props.ch1ToCh2);
+    },
+        [props]);
 
     return (
         <div>
             <h1>Child2 Component</h1>
-            <p>Child data in child2 component: {props.ch1ToCh2}</p>
+            <p>Child data in child2 component: {child2Data}</p>
         </div>
     );
 };
 
 export default Child2;
+
+
+// const Child2 = (props) => {
+
+//     return (
+//         <div>
+//             <h1>Child2 Component</h1>
+//             <p>Child data in child2 component: {props.ch1ToCh2}</p>
+//         </div>
+//     );
+// };
+
+// export default Child2;
 
 // import { useState } from "react";
 
