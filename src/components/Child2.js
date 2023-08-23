e0e6e69899c24904a80cd6e3cb01851af752c2fc
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-23 11:49:19 +0530

    react

--- src/components/Child2.js
+++ src/components/Child2.js
@@ -1,19 +1,42 @@
+// import { useEffect, useState } from "react";
+
 import { useEffect, useState } from "react";
+
+// const Child2 = (props) => {
+
+//     const [child2Data, setChild2Data] = useState('');
+
+//     useEffect(() => {
+//         console.log(props);
+//         setChild2Data(props.ch1ToCh2);
+//     },
+//         [props]);
+
+//     return (
+//         <div>
+//             <h1>Child2 Component</h1>
+//             <p>Child data in child2 component: {child2Data}</p>
+//         </div>
+//     );
+// };
+
+// export default Child2;
 
 const Child2 = (props) => {
 
-    const [child2Data, setChild2Data] = useState('');
+    const [ch1DataInCh2, setCh1DataInCh2] = useState('');
+
+    // useEffect(() => { }, []);
 
     useEffect(() => {
-        console.log(props);
-        setChild2Data(props.ch1ToCh2);
+        setCh1DataInCh2(props.ch1ToCh2);
     },
         [props]);
 
     return (
         <div>
             <h1>Child2 Component</h1>
-            <p>Child data in child2 component: {child2Data}</p>
+            <p>Child data in child2 component: {ch1DataInCh2}</p>
         </div>
     );
 };
@@ -21,17 +44,7 @@ const Child2 = (props) => {
 export default Child2;
 
 
-// const Child2 = (props) => {
 
-//     return (
-//         <div>
-//             <h1>Child2 Component</h1>
-//             <p>Child data in child2 component: {props.ch1ToCh2}</p>
-//         </div>
-//     );
-// };
-
-// export default Child2;
 
 // import { useState } from "react";
 
